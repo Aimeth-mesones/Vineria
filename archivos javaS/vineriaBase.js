3e7a3c685e49295d0fca8bd2c3b0135edfc11936
MAITAYORMARIS <yormarism@gmail.com>
2023-04-06 19:12:10 -0300

    agregando datos de vinos

--- archivos javaS/vineriaBase.js
+++ archivos javaS/vineriaBase.js
@@ -1,208 +1,196 @@
 {
-    "vinos"=[
+    "vinos" = [
         {//Victoria
-            "id":1,
-            "imagen":"",//nombre de la imagen
-            "nombre":"",//nombre del vino
-            "descripcion":"",//descripcion corta para la pag de home
+            "id": 1,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
             "precio":,//precio real
-            "bodega":"",//nombre de la bodega que lo elabora
-            "region":"",// de donde es el vino
-            "viñedo":"",//nombre del viñedo
-            "crianza":"",//
-            "notas de cata":"",// datos de color, sabor y textura
-            "tipo de productos":"",//vino espumante, blanco o tinto
-            "varietal":"",//tipo de uva
-            "presentacion":"",//medida o cantidad de la botella
-            "tiempo de crianza":"",   
-            "info" :"",//informacion ampliada del vino
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
-            "id":2,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 2,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica  
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
-            "id":3,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 3,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {//Yormaris
-            "id":4,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 4,
+            "imagen": "trapiche-reserva-chardonnay.jpg",
+            "nombre": "Trapiche Reserva",
+            "precio": 2307,
+            "bodega": "Trapiche",
+            "region": "Mendoza",
+            "viñedo": "Valle de Maipu",
+            "notas de cata": "De color amarillo verdoso, este vino expresa aromas a peras y pastel de manzana, con un dejo de pan tostado. En boca presenta una buena frescura, y es vibrante y mineral con un final muy untuoso.",
+            "tipo de productos": "Vino Blanco",
+            "varietal": "Chardonnay",
+            "presentacion": "750ml",
+            "crianza": "Roble francés y americano",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "6 meses",  //cuanto tiempo pasa en barrica 
+            "info": "Trapiche Reserva es la línea emblemática de la bodega que nace al pie de la Cordillera de Los Andes para transformarse en la mejor expresión de todo lo bueno que Trapiche tiene para dar. Un vino que se caracteriza por su delicadeza, armonía y complejidad, otorgadas por su crianza en roble francés y americano a lo largo de 6 meses. Ideal para acompañar una bondiola de cerdo asada con manzanas verdes y crema de limon. Marida a la perfección con fondue de queso, e incluso ensaladas de verano; se recomienda beberlo entre 4 y 6°C",
         },
         {
-            "id":5,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 5,
+            "imagen": "EL-ESTECO-Blanc-de-Blancs.png",
+            "nombre": "El Esteco Blanc de Blancs",
+            "precio": 14631,
+            "bodega": "El Esteco",
+            "region": "Salta",
+            "viñedo": "Valle Calchaquí",
+            "notas de cata": "De color amarillo intenso profundo con notas verdosas. Aroma de fruta tropical pero fresca, mineral, muy elegante. Excelente balance de acidez y dulzura en boca.",
+            "tipo de productos": "Vino Blanco",
+            "varietal": "Chardonnay 40%, Marssane 20%,Roussane 20%, Torrontés 15%, Viogner 5%",
+            "presentacion": "6 x 750ml",
+            "crianza": "",
+            "tiempo de crianza": "2 meses en estiba",
+            "info": "Proviene de las fincas de la Bodega Esteco más selectas otorgando características de un terroir único. No solo se trata de un gran vino, se trata sobre la historia de la historia de Esteco en Cafayate. Sobre sus viñedos que rodean a la bodega y del cuidado de una tierra única. Marida muy bien con Mero a la plancha, papines confitados con eneldo y oregano, se recomienda beberlo entre 11º a 13ºC.",
         },
         {
-            "id":6,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 6,
+            "imagen": "coleccionprivadasauvinionblanc.png",
+            "nombre": "Coleccion Privada",
+            "precio": 605,
+            "bodega": "Navarro Correas",
+            "region": "Mendoza",
+            "viñedo": "Lujan de Cuyo y Valle de Uco",
+            "notas de cata": "De color amarillo pálido con reflejos verdes y plata. Aromas a pomelo rosado, maracuyá, espárrago, notas de flores blancas. En boca, gran balance entre la fruta, las notas florales y la acidez. Fresco y persistente. ",
+            "tipo de productos": "Vino Blanco",
+            "varietal": "Sauvignon Blanc",
+            "presentacion": "750ml",
+            "crianza": "",
+            "tiempo de crianza": "6 meses",
+            "info": "Colección Privada es la línea que nos recuerda que el arte de hacer nos permite explorar y encontrar siempre nuevas y mejores expresiones. Vinos de estilo frutado, textura aterciopelada y larga presencia en boca distinguen a esta línea de varietales. Ideal para acompañar pescados y mariscos. Platos con alta acidez, ceviche, encurtidos y vinagretas. Servir a 12°C.",
         },
         {//Vasty
-            "id":7,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 7,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
-            "id":8,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 8,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
-            "id":9,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 9,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {//Samantha
-            "id":10,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 10,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
-            "id":11,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 11,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
-            "id":12,
-            "imagen":"",
-            "nombre":"",
-            "descripcion":"",
-            "precio":,
-            "bodega":"",
-            "region":"",
-            "viñedo":"",
-            "crianza":"",
-            "notas de cata":"",
-            "tipo de productos":"",
-            "varietal":"",
-            "presentacion":"",
-            "tiempo de crianza":"",   
-            "info" :"",
+            "id": 12,
+            "imagen": "",//nombre de la imagen
+            "nombre": "",//nombre del vino
+            "precio":,//precio real
+            "bodega": "",//nombre de la bodega que lo elabora
+            "region": "",// de donde es el vino (provincia)
+            "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
+            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo de productos": "",//vino espumante, blanco o tinto
+            "varietal": "",//tipo de uva
+            "presentacion": "",//medida o cantidad de la botella
+            "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
+            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         }
     ]
 }
