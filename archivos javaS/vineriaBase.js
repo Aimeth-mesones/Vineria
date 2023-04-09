96873705a2760a8afc97822268ce3bcd7de266e0
MAITAYORMARIS <yormarism@gmail.com>
2023-04-09 16:35:23 -0300

    creando filtro JS por tipo de vino

--- archivos javaS/vineriaBase.js
+++ archivos javaS/vineriaBase.js
@@ -1,5 +1,5 @@
-{
-    "vinos" = [
+var dataVitis={
+    "vinos" :[
         {
             "id": 1,
             "imagen": "Vino\ Tinto\ Rutini3.jpg",
@@ -8,12 +8,12 @@
             "bodega": "La Rural",
             "region": "Mendoza",
             "viñedo": "Valle de Uco",
-            "notas de cata": "De color Rojo rubí profundo. El dúo de cepas de origen francés da como resultado un tinto armonioso y equilibrado: la Cabernet Sauvignon entrega su carácter corpóreo y pleno, y la Malbec matiza el corte con taninos suaves y dulces, realzando una combinación única",
-            "tipo de productos": "Vino Tinto",
+            "notas": "De color Rojo rubí profundo. El dúo de cepas de origen francés da como resultado un tinto armonioso y equilibrado: la Cabernet Sauvignon entrega su carácter corpóreo y pleno, y la Malbec matiza el corte con taninos suaves y dulces, realzando una combinación única",
+            "tipo": "Tinto",
             "varietal": "Malbec",
             "presentacion": "750 ml",
             "crianza": "Roble Frances",
-            "tiempo de crianza": "12 meses", 
+            "tiempo": "12 meses", 
             "info": "Ideal para disfrutar en compañía.,Carnes rojas asadas,tabla de quesos semiduros: Pategras,Fontina,Gruyere,Emmental,pastel de carne,pastas rellenas: sorrentinos,agnolottis,lasagna y comidas criollas: empanadas y todo tipo de cazuelas y Temperatura ideal de consumo: 15 °C",
         },
         {
@@ -24,12 +24,12 @@
             "bodega": "Trapiche",
             "region": "Mendoza",
             "viñedo": "Finca Las Piedras, Los Árboles",
-            "notas de cata": "De color rojo púrpura, este vino presenta un fino bouquet con aromas de cerezas negras, ciruelas, notas de lavanda, especias y minerales. En boca es voluminoso, exhibiendo una elegante personalidad.",
-            "tipo de productos": "Vino Tinto",
+            "notas": "De color rojo púrpura, este vino presenta un fino bouquet con aromas de cerezas negras, ciruelas, notas de lavanda, especias y minerales. En boca es voluminoso, exhibiendo una elegante personalidad.",
+            "tipo": "Tinto",
             "varietal": "Malbec",
             "presentacion": "750 ml",
             "crianza": "Roble francés",
-            "tiempo de crianza": "18 meses", 
+            "tiempo": "18 meses", 
             "info": "Trapiche Gran Medalla nace para conmemorar los 125 años de la bodega. Fiel a su espíritu explorador y en la constante búsqueda de nuevos terroir, los enólogos de Trapiche llegaron al Valle de Uco, Mendoza, donde se encontraron vides que entregan ejemplares de excepción.",
         },
         {
@@ -40,12 +40,12 @@
             "bodega": "El Esteco",
             "region": "Salta",
             "viñedo": "Cafayate",
-            "notas de cata": "Color: Rojo con reflejos rubíes, profundo y fondo negro. Limpio y brillante. Lágrimas muy coloreadas. Aroma: Concentrado. Especiado, se destacan los pimientos morrones. Roble, chocolate negro, dulce de cassis. Limpio y persistente. Característico Cabernet Sauvignon Cafayateño. Sabor: Taninos de gran envergadura, maduros y sensaciones de tiza. Armónico con percepción de frutas negras y chocolate. Roble acompañando al vino. Rico. Persistencia larga y elegante.",
-            "tipo de productos": "Vino Tinto",
+            "notas": "Color: Rojo con reflejos rubíes, profundo y fondo negro. Limpio y brillante. Lágrimas muy coloreadas. Aroma: Concentrado. Especiado, se destacan los pimientos morrones. Roble, chocolate negro, dulce de cassis. Limpio y persistente. Característico Cabernet Sauvignon Cafayateño. Sabor: Taninos de gran envergadura, maduros y sensaciones de tiza. Armónico con percepción de frutas negras y chocolate. Roble acompañando al vino. Rico. Persistencia larga y elegante.",
+            "tipo": "Tinto",
             "varietal": "Cabernet Sauvignon",
             "presentacion": "750 ml",
             "crianza": "Roble Frances",
-            "tiempo de crianza": "15 meses",  
+            "tiempo": "15 meses",  
             "info": "FINCAS NOTABLES es una línea de “Single Vineyards” que proviene de cuarteles especiales en nuestras fincas propias. Estos vinos exclusivos transmiten la perfecta fusión entre el cuidado de los viñedos, la sabiduría de la naturaleza y la sapiencia del trabajo.",
         },
         {//Yormaris
@@ -56,12 +56,12 @@
             "bodega": "Trapiche",
             "region": "Mendoza",
             "viñedo": "Valle de Maipu",
-            "notas de cata": "De color amarillo verdoso, este vino expresa aromas a peras y pastel de manzana, con un dejo de pan tostado. En boca presenta una buena frescura, y es vibrante y mineral con un final muy untuoso.",
-            "tipo de productos": "Vino Blanco",
+            "notas": "De color amarillo verdoso, este vino expresa aromas a peras y pastel de manzana, con un dejo de pan tostado. En boca presenta una buena frescura, y es vibrante y mineral con un final muy untuoso.",
+            "tipo": "Blanco",
             "varietal": "Chardonnay",
             "presentacion": "750ml",
             "crianza": "Roble francés y americano",// tipo de material donde se almacena (tipo de madera de la barrica)
-            "tiempo de crianza": "6 meses",  //cuanto tiempo pasa en barrica 
+            "tiempo": "6 meses",  //cuanto tiempo pasa en barrica 
             "info": "Trapiche Reserva es la línea emblemática de la bodega que nace al pie de la Cordillera de Los Andes para transformarse en la mejor expresión de todo lo bueno que Trapiche tiene para dar. Un vino que se caracteriza por su delicadeza, armonía y complejidad, otorgadas por su crianza en roble francés y americano a lo largo de 6 meses. Ideal para acompañar una bondiola de cerdo asada con manzanas verdes y crema de limon. Marida a la perfección con fondue de queso, e incluso ensaladas de verano; se recomienda beberlo entre 4 y 6°C",
         },
         {
@@ -72,12 +72,12 @@
             "bodega": "El Esteco",
             "region": "Salta",
             "viñedo": "Valle Calchaquí",
-            "notas de cata": "De color amarillo intenso profundo con notas verdosas. Aroma de fruta tropical pero fresca, mineral, muy elegante. Excelente balance de acidez y dulzura en boca.",
-            "tipo de productos": "Vino Blanco",
+            "notas": "De color amarillo intenso profundo con notas verdosas. Aroma de fruta tropical pero fresca, mineral, muy elegante. Excelente balance de acidez y dulzura en boca.",
+            "tipo": "Blanco",
             "varietal": "Chardonnay 40%, Marssane 20%,Roussane 20%, Torrontés 15%, Viogner 5%",
             "presentacion": "6 x 750ml",
             "crianza": "Roble Frances",
-            "tiempo de crianza": "2 meses en estiba",
+            "tiempo": "2 meses en estiba",
             "info": "Proviene de las fincas de la Bodega Esteco más selectas otorgando características de un terroir único. No solo se trata de un gran vino, se trata sobre la historia de la historia de Esteco en Cafayate. Sobre sus viñedos que rodean a la bodega y del cuidado de una tierra única. Marida muy bien con Mero a la plancha, papines confitados con eneldo y oregano, se recomienda beberlo entre 11º a 13ºC.",
         },
         {
@@ -88,108 +88,108 @@
             "bodega": "Navarro Correas",
             "region": "Mendoza",
             "viñedo": "Lujan de Cuyo y Valle de Uco",
-            "notas de cata": "De color amarillo pálido con reflejos verdes y plata. Aromas a pomelo rosado, maracuyá, espárrago, notas de flores blancas. En boca, gran balance entre la fruta, las notas florales y la acidez. Fresco y persistente. ",
-            "tipo de productos": "Vino Blanco",
+            "notas": "De color amarillo pálido con reflejos verdes y plata. Aromas a pomelo rosado, maracuyá, espárrago, notas de flores blancas. En boca, gran balance entre la fruta, las notas florales y la acidez. Fresco y persistente. ",
+            "tipo": "Blanco",
             "varietal": "Sauvignon Blanc",
             "presentacion": "750ml",
             "crianza": "Roble",
-            "tiempo de crianza": "6 meses",
+            "tiempo": "6 meses",
             "info": "Colección Privada es la línea que nos recuerda que el arte de hacer nos permite explorar y encontrar siempre nuevas y mejores expresiones. us etiquetas exhiben obras de prestigiosos y emergentes artistas argentinos, creando así una amplia colección de obras de arte y vinos. Vinos de estilo frutado, textura aterciopelada y larga presencia en boca distinguen a esta línea de varietales. Ideal para acompañar pescados y mariscos. Platos con alta acidez, ceviche, encurtidos y vinagretas. Servir a 12°C.",
         },
         {//Vasty
             "id": 7,
             "imagen": "",//nombre de la imagen
             "nombre": "",//nombre del vino
-            "precio":,//precio real
+            "precio":0,//precio real
             "bodega": "",//nombre de la bodega que lo elabora
             "region": "",// de donde es el vino (provincia)
             "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
-            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
-            "tipo de productos": "",//vino espumante, blanco o tinto
+            "notas": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo": "Espumante",//vino espumante, blanco o tinto
             "varietal": "",//tipo de uva
             "presentacion": "",//medida o cantidad de la botella
             "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
-            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "tiempo": "",  //cuanto tiempo pasa en barrica 
             "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
             "id": 8,
             "imagen": "",//nombre de la imagen
             "nombre": "",//nombre del vino
-            "precio":,//precio real
+            "precio":0,//precio real
             "bodega": "",//nombre de la bodega que lo elabora
             "region": "",// de donde es el vino (provincia)
             "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
-            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
-            "tipo de productos": "",//vino espumante, blanco o tinto
+            "notas": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo": "Espumante",//vino espumante, blanco o tinto
             "varietal": "",//tipo de uva
             "presentacion": "",//medida o cantidad de la botella
             "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
-            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "tiempo": "",  //cuanto tiempo pasa en barrica 
             "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
             "id": 9,
             "imagen": "",//nombre de la imagen
             "nombre": "",//nombre del vino
-            "precio":,//precio real
+            "precio":0,//precio real
             "bodega": "",//nombre de la bodega que lo elabora
             "region": "",// de donde es el vino (provincia)
             "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
-            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
-            "tipo de productos": "",//vino espumante, blanco o tinto
+            "notas": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo": "Espumante",//vino espumante, blanco o tinto
             "varietal": "",//tipo de uva
             "presentacion": "",//medida o cantidad de la botella
             "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
-            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "tiempo": "",  //cuanto tiempo pasa en barrica 
             "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {//Samantha
             "id": 10,
             "imagen": "",//nombre de la imagen
             "nombre": "",//nombre del vino
-            "precio":,//precio real
+            "precio":0,//precio real
             "bodega": "",//nombre de la bodega que lo elabora
             "region": "",// de donde es el vino (provincia)
             "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
-            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
-            "tipo de productos": "",//vino espumante, blanco o tinto
+            "notas": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo": "Blanco",//vino espumante, blanco o tinto
             "varietal": "",//tipo de uva
             "presentacion": "",//medida o cantidad de la botella
             "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
-            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "tiempo": "",  //cuanto tiempo pasa en barrica 
             "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
             "id": 11,
             "imagen": "",//nombre de la imagen
             "nombre": "",//nombre del vino
-            "precio":,//precio real
+            "precio":0,//precio real
             "bodega": "",//nombre de la bodega que lo elabora
             "region": "",// de donde es el vino (provincia)
             "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
-            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
-            "tipo de productos": "",//vino espumante, blanco o tinto
+            "notas": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo": "Tinto",//vino espumante, blanco o tinto
             "varietal": "",//tipo de uva
             "presentacion": "",//medida o cantidad de la botella
             "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
-            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "tiempo": "",  //cuanto tiempo pasa en barrica 
             "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         },
         {
             "id": 12,
             "imagen": "",//nombre de la imagen
             "nombre": "",//nombre del vino
-            "precio":,//precio real
+            "precio":0,//precio real
             "bodega": "",//nombre de la bodega que lo elabora
             "region": "",// de donde es el vino (provincia)
             "viñedo": "",//donde esta ubicado el viñedo o bodega(parte de la provincia)
-            "notas de cata": "",// descripcion corta para la pag de home datos de color, sabor y textura
-            "tipo de productos": "",//vino espumante, blanco o tinto
+            "notas": "",// descripcion corta para la pag de home datos de color, sabor y textura
+            "tipo": "Espumante",//vino espumante, blanco o tinto
             "varietal": "",//tipo de uva
             "presentacion": "",//medida o cantidad de la botella
             "crianza": "",// tipo de material donde se almacena (tipo de madera de la barrica)
-            "tiempo de crianza": "",  //cuanto tiempo pasa en barrica 
+            "tiempo": "",  //cuanto tiempo pasa en barrica 
             "info": "",//informacion ampliada del vino,datos de la bodega,maridaje
         }
     ]
